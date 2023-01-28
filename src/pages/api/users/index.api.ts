1c785d10c441c0c83e147a258526da0dafbb821a
Saron Medeiros Philippi <saron.ph@gmail.com>
2023-01-28 18:03:43 -0300

    added next auth login

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma';
+import { setCookie } from 'nookies';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
@@ -13,13 +14,31 @@ export default async function handler(
     case 'POST':
       const { name, username } = req.body;
 
+      const userExists = await prisma.user.findUnique({
+        where: {
+          username,
+        },
+      });
+
+      if (userExists) {
+        return res.status(400).json({
+          message: 'User already exists',
+        });
+      }
+
       const user = await prisma.user.create({
         data: {
           name,
           username,
         },
       });
-      res.status(201).end(user);
+
+      setCookie({ res }, '@ignitecall:userId', user.id, {
+        maxAge: 60 * 60 * 24 * 7,
+        path: '/',
+      });
+
+      res.status(201).json(user);
       break;
     case 'PUT':
       break;
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,6 +1,7 @@
 import { api } from '@/lib/axios';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react';
+import { AxiosError } from 'axios';
 import { useRouter } from 'next/router';
 import { ArrowRight } from 'phosphor-react';
 import { useEffect } from 'react';
@@ -45,7 +46,14 @@ export default function Register() {
         name: data.name,
         username: data.username,
       });
+
+      await router.push('/register/connect-calendar');
     } catch (error) {
+      if (error instanceof AxiosError && error?.response?.data.message) {
+        alert(error.response.data.message);
+        return;
+      }
+
       console.log(error);
     }
   }
